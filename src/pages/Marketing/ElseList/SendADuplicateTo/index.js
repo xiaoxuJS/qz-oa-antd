af2e545a543c4aa86db0d91e14ece36790cf45c9
18749875170@163.com <18749875170@163.com>
2021-06-21 17:09:54 +0800

    分页

--- src/pages/Marketing/ElseList/SendADuplicateTo/index.js
+++ src/pages/Marketing/ElseList/SendADuplicateTo/index.js
@@ -33,6 +33,41 @@ const SendADuplicateTo = () => {
     useEffect(() => {
         listFun();
     }, [listFun]);
+    //分页
+    const pagination = {
+        //是否可以改变pagesize//
+        showSizeChanger: true,
+        //是否可以快速跳转到某页
+        showQuickJumper: true,
+        //展示一共有多少条数据//
+        showTotal: () => `共${total}条`,
+        //每页条数
+        pageSize: size,
+        //当前页数
+        current: currentPage,
+        //数据总数
+        total: total,
+        //pageSize 变化的回调
+        onShowSizeChange: (current, pageSize) => changePageSize(pageSize, current),
+        //页码改变的回调，参数是改变后的页码及每页条数
+        onChange: (current) => changePage(current),
+    }
+    //改变每页条数
+    const changePageSize = (pageSizeFun, current) => {
+        const parames = {
+            currentPage: current,
+            pageSize: pageSizeFun
+        }
+        listFun(parames);
+    }
+    //改变页数
+    const changePage = (current) => {
+        const parames = {
+            currentPage: current,
+            size
+        }
+        listFun(parames);
+    }
     const columns = [
         {
             title: '编号',
@@ -70,7 +105,7 @@ const SendADuplicateTo = () => {
             title='抄送财务'
         ></PageHeader>
         <Spin tip="Loading..." spinning={spinning}>
-            <Table columns={columns} dataSource={listData} rowKey='id' />
+            <Table columns={columns} dataSource={listData} rowKey='id' pagination = {pagination} />
         </Spin>
     </SendADuplicateToAll>
 }
